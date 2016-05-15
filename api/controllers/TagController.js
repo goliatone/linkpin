9eef6ef4aabca94bf66f3dc845fad3bdf04b1502
goliatone <burgosemi@gmail.com>
2016-05-15 18:25:40 -0400

    Added TagController;

---
+++ api/controllers/TagController.js
@@ -0,0 +1,10 @@
+/**
+ * TagController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+};
