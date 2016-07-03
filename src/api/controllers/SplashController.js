b20d9d9fa2a6a59f57f9a70e353b00131cf97964
goliatone <burgosemi@gmail.com>
2016-07-03 18:42:44 -0400

    Added SplashController;

---
+++ src/api/controllers/SplashController.js
@@ -0,0 +1,12 @@
+/**
+ * SplashController
+ *
+ * @description :: Server-side logic for managing splashes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	index: function(req, res){
+        res.render('splash/index');
+    },
+};
