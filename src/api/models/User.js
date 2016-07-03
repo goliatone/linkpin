9be8a57a47243aa7da448a6d2a41c3c93457aac6
goliatone <burgosemi@gmail.com>
2016-07-03 17:55:38 -0400

    User: adding encryption

--- src/api/models/User.js
+++ src/api/models/User.js
@@ -1,25 +1,80 @@
+"use strict";
+
+var bcrypt = require('bcrypt');
+
 var User = {
-  // Enforce model schema in the case of schemaless databases
-  schema: true,
-  identity: 'User',
-  attributes: {
-    username  : { type: 'string', unique: true },
-    email     : { type: 'email',  unique: true },
-    // passports : { collection: 'Passport', via: 'user' },
-    /////////////////////////
-    /// RELATIONSHIPS
-    /////////////////////////
-    //ONE-TO-MANY
-    links: {
-        collection: 'link',
-        via: 'owner'
+    // Enforce model schema in the case of schemaless databases
+    schema: true,
+    identity: 'User',
+    attributes: {
+        username  : {
+            type: 'string',
+            unique: true,
+            required: true
+        },
+        email : {
+            type: 'email',
+            unique: true,
+            required: true
+        },
+        password: {
+            type: 'string',
+            required: true
+        },
+        // passports : { collection: 'Passport', via: 'user' },
+        /////////////////////////
+        /// RELATIONSHIPS
+        /////////////////////////
+        //ONE-TO-MANY
+        links: {
+            collection: 'link',
+            via: 'owner'
+        },
+        //MANY-TO-ONE
+        notes: {
+            collection: 'note',
+            via: 'author'
+        },
+        matchesPassword: function (password, cb) {
+           return bcrypt.compare(password, this.password, cb);
+       },
+       toJSON: function(){
+         var obj = this.toObject();
+         delete obj._csrf;
+         delete obj.password;
+         delete obj.passwordConfirm;
+         return obj;
+       },
     },
-    //MANY-TO-ONE
-    notes: {
-        collection: 'note',
-        via: 'author'
+    beforeValidate: function (values, next) {
+        if (values.email) values.email = values.email.toLowerCase();
+        next();
+    },
+    beforeCreate: function (values, next) {
+
+        if(!values.password || values.password !== values.passwordConfirm){
+            return next({err: ['Password does not match password confirmation']});
+        }
+
+        bcrypt.hash(values.password, sails.config.crypto.workFactor, function(err, hash) {
+            if(err) return next(err);
+            values.password = hash;
+            delete values.passwordConfirm;
+            next();
+        });
+    },
+    beforeUpdate: function (values, next) {
+        if(values.password) {
+            bcrypt.hash(values.password, sails.config.crypto.workFactor, function(err, hash) {
+                if(err) return next(err);
+                values.password = hash;
+                delete values.passwordConfirm;
+                next();
+            });
+        } else {
+            return next();
+        }
     }
-  }
 };
 
 module.exports = User;
