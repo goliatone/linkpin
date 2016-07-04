444a57fd4878e7d7ba4f0afe63d702f0cf0f97e2
goliatone <burgosemi@gmail.com>
2016-07-04 19:39:40 -0400

    JS: app, use describe endpoint

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -10,13 +10,41 @@ $(function(){
             console.warn(d);
             location.href = '/site';
         }).fail(function(e){
-
+            console.error(e);
         });
     });
 
     $('#search-btn').click(function(){
         var term = $('input[name=search]').val();
         location.href = '/site/search?q=' + encodeURIComponent(term);
+    });
+
+    $('input[name=url]', '#submit-form').blur(function() {
+        console.log('out', $(this).val());
+        $.ajax({
+            url:'/site/describe',
+            method: 'GET',
+            data:{
+                url: $(this).val()
+            }
+        }).done(function(d){
+            console.warn(d);
+            if(!d || !d.success) return;
+            if(d.value.description){
+                $('textarea[name=description]', '#submit-form').text(d.value.description);
+            }
+            if(d.value.title){
+                $('input[name=title]', '#submit-form').val(d.value.description);
+            }
+
+            if(d.value.tags){
+                if(d.value.tags.length) $('input[name=tags]', '#submit-form').val(d.value.tags.join(','));
+                else $('input[name=tags]', '#submit-form').focus();
+            }
+
+
+
+        });
     });
 });
 
