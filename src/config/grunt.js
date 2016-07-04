45407c9c2f542142a36265f98f3db68b18b1b99d
goliatone <burgosemi@gmail.com>
2016-07-04 12:23:43 -0400

    config: adding grunt conf

---
+++ src/config/grunt.js
@@ -0,0 +1,3 @@
+module.exports.grunt = {
+    _hookTimeout: 1 * 60 * 1000
+};
--- src/config/policies.js
+++ src/config/policies.js
@@ -27,7 +27,7 @@ module.exports.policies = {
   ***************************************************************************/
   // '*': true,
   '*': ['sessionAuth'],
-  'SplashController':{
+  'SplashController': {
     'index': true
   },
   'AuthController': {
