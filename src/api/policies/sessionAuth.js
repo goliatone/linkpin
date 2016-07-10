87ddfb257252b895349860e6152a46d68f9e7c67
goliatone <burgosemi@gmail.com>
2016-07-10 01:38:13 -0400

    ...

--- src/api/policies/sessionAuth.js
+++ src/api/policies/sessionAuth.js
@@ -14,7 +14,9 @@ module.exports = function(req, res, next) {
   if (req.session.authenticated) {
     return next();
   }
-
+  //TODO: We should capture the curent URL and store it in next:
+  //req.session.next = req.url;
+  
   // User is not allowed
   // (default res.forbidden() behavior can be overridden in `config/403.js`)
   // return res.forbidden('You are not permitted to perform this action.');
