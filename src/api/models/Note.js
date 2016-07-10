af2e27cf1f7d017a4af7d5533c17ce0d8db35506
goliatone <burgosemi@gmail.com>
2016-07-10 01:37:07 -0400

    Note: store author name before save

--- src/api/models/Note.js
+++ src/api/models/Note.js
@@ -2,6 +2,7 @@
 var Note = {
     autoPK: true,
     identity: 'Note',
+    autoCreatedAt: true,
     attributes: {
         body: {
             type: 'text'
@@ -13,10 +14,34 @@ var Note = {
         link: {
             model: 'link'
         },
-        //ONE-TO-ONE
+        //ONE-TO-MANY
         author: {
             model: 'user'
+        },
+        authorName:{
+            type: 'string'
         }
+    },
+    beforeCreate: function(values, next){
+        console.log(
+            'BEFORE CREATE',
+            values
+        );
+        if(values.author){
+            if(values.author.username){
+                values.authorName = values.author.username;
+                next();
+            } else {
+                User.findOne(values.author).then(function(author){
+                    values.authorName = author.username;
+                    console.log('HERE MOTHERFUCKERS', author);
+                    next();
+                }).catch(function(err){
+                    console.log('HERE', err);
+                    next();
+                })
+            }
+        } else next();
     }
 };
 module.exports = Note;
