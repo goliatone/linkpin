d01c6387acdba8e2ad6f0f5be2902c0dd09389c7
goliatone <burgosemi@gmail.com>
2016-07-16 22:10:39 -0400

    AuthController: Added getToken

--- src/api/controllers/AuthController.js
+++ src/api/controllers/AuthController.js
@@ -8,9 +8,25 @@
 var Promise  = require('bluebird');
 
 module.exports = {
+    getToken: function(req, res){
+        //TODO: this should be handled by basicAuth, here just return token.
+
+        User.findOne({email: req.body.email}).populate('tokens').exec(function (error, user) {
+            if (error || !user) return res.forbidden();
+
+            console.log('user', user);
+
+            user.matchesPassword(req.body.password, function(err, match){
+                if(err || !match) return res.forbidden();
+                var token = user.tokens[0];
+                //TODO: this is just to get it working for now...
+                if(!token) return res.forbidden();
+                return res.jsonx({token: token.accessToken});
+            });
+        });
+    },
     login: function (req, res) {
         if (req.method === 'POST') {
-
             User.findOne({email: req.body.email}).exec(function (error, user) {
                 if (error || !user) {
                     return res.redirect('/');
@@ -26,6 +42,10 @@ module.exports = {
                         gravatarUrl: user.gravatarUrl
                     };
                     req.session.user = user;
+
+                    if (req.wantsJSON) {
+                        return res.jsonx({token: user.tokens});
+                    }
 
                     res.redirect('/site');
                 });
@@ -50,7 +70,7 @@ module.exports = {
                         gravatarUrl: user.gravatarUrl
                     };
                 req.session.user = user;
-                
+
                 res.redirect('/site');
             });
         }
