87d4d0335264be69d31ded5ed3c9f5ee4ce5ab0a
goliatone <burgosemi@gmail.com>
2016-07-17 18:19:58 -0400

    ...

--- src/api/responses/notFound.js
+++ src/api/responses/notFound.js
@@ -58,7 +58,9 @@ module.exports = function notFound (data, options) {
 
   // If no second argument provided, try to serve the default view,
   // but fall back to sending JSON(P) if any errors occur.
-  else return res.view('404', { data: data }, function (err, html) {
+  else return res.view('404', {
+      data: data 
+  }, function (err, html) {
 
     // If a view error occured, fall back to JSON(P).
     if (err) {
@@ -79,4 +81,3 @@ module.exports = function notFound (data, options) {
   });
 
 };
-
